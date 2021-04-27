9710dc0f5244b983b08b0973305fa28e413bde8b
Brian Le <brian.le@grabtaxi.com>
2021-04-27 16:24:16 +0800

    cm

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-import queue
 import collections
+
 
 def binary_search(list, key):
     low = 0
@@ -19,12 +19,14 @@ def binary_search(list, key):
     return -1
 # print(binary_search([1,2,3,4,5], 6))
 
+
 graph = {
     'a': ['b', 'c'],
     'b': ['a', 'c'],
     'c': ['b', 'a', 'd', 'e'],
     'd': ['c'],
     'e': ['c']}
+
 
 def bfs(graph):
     reached = set()
@@ -42,6 +44,7 @@ def bfs(graph):
 
     print(reached)
 
+
 def dfs(graph):
     reached = set()
     stack = collections.deque()
@@ -56,9 +59,10 @@ def dfs(graph):
 
     print(reached)
 
+
 def partition(array, left, right):
     pivot = array[right]
-    i = left # i is the last position where arra[i] > pivot
+    i = left  # i is the last position where arra[i] > pivot
     for j in range(left, right):
         if array[j] <= pivot:
             array[i], array[j] = array[j], array[i]  # swap arr[i] arr[j]
@@ -67,14 +71,17 @@ def partition(array, left, right):
     array[i], array[right] = array[right], array[i]
     return i
 
+
 def quicksort(array):
     quicksort_helper(array, 0, len(array) - 1)
+
 
 def quicksort_helper(array, left, right):
     if left < right:
         pivotIndex = partition(array, left, right)
         quicksort_helper(array, left, pivotIndex - 1)
-        quicksort_helper(array, pivotIndex + 1, right) 
+        quicksort_helper(array, pivotIndex + 1, right)
+
 
 def insertion_sort(array):
     for i in range(len(array)):
@@ -85,6 +92,7 @@ def insertion_sort(array):
     return array
 # print(insertion_sort([5, 7, 2, 3, 4, 1, 6]))
 
+
 def selection_sort(array):
     for i in range(len(array)):
         minIndex = -1
@@ -93,7 +101,8 @@ def selection_sort(array):
                 minIndex = j
         array[i], array[minIndex] = array[minIndex], array[i]
     return array
-# print(selection_sort([5, 7, 2, 3, 4, 1, 6]))    
+# print(selection_sort([5, 7, 2, 3, 4, 1, 6]))
+
 
 def greatestCommonDivisor(a, b):
     if a == 0:
@@ -103,6 +112,7 @@ def greatestCommonDivisor(a, b):
 
     remainder = a % b
     return greatestCommonDivisor(b, remainder)
+
 
 def longest_common_subsequence(str1, str2):
     if not str1 or not str2:
@@ -117,22 +127,24 @@ def longest_common_subsequence(str1, str2):
         return max(longest_common_subsequence(newStr1, str2), longest_common_subsequence(str1, newStr2))
 # print(longest_common_subsequence('abdjsh', 'ads')) # a d s
 
+
 def longest_common_subsequence_dp(str1, str2):
     res = 0
     dp = [[0] * (len(str2) + 1) for _ in range(len(str1) + 1)]
 
     for i in range(1, len(str1)+1):
-        for j in range(1,len(str2)+1):
+        for j in range(1, len(str2)+1):
             if str1[i-1] == str2[j-1]:
                 dp[i][j] = 1 + dp[i-1][j-1]
             else:
                 dp[i][j] = max(dp[i][j-1], dp[i-1][j])
-            res = max(res, dp[i][j])   
+            res = max(res, dp[i][j])
     print(dp)
     return res
 # print(longest_common_subsequence_dp('fort', 'fosh')) # a d s
 
-def longest_common_substring(str1, str2):
+
+def longest_common_substring_dp(str1, str2):
     res = 0
     dp = [[0] * len(str2) for _ in range(len(str1))]
 
@@ -142,11 +154,12 @@ def longest_common_substring(str1, str2):
                 dp[i][j] = 1 + (dp[i-1][j-1] if i > 0 and j > 0 else 0)
             else:
                 dp[i][j] = 0
-            res = max(res, dp[i][j])   
+            res = max(res, dp[i][j])
     print(dp)
     return res
 # print(longest_common_substring('fish', 'hish'))
 # print(longest_common_substring('fisdishe', 'hishefcd'))
+
 
 def findKthLargest(arr, k):
     left = 0
@@ -164,6 +177,7 @@ def findKthLargest(arr, k):
     return -1
 # print(findKthLargest([5,7,2,3,4, 1,6], 3))
 
+
 def merge2sorted(arr1, arr2):
     newArr = []
     n1 = len(arr1)
@@ -174,22 +188,24 @@ def merge2sorted(arr1, arr2):
     while i + j < n1 + n2:
         if i < n1 and j < n2:
             if arr1[i] <= arr2[j]:
-                newArr.append(arr1[i])    
-                i+=1
+                newArr.append(arr1[i])
+                i += 1
             else:
                 newArr.append(arr2[j])
-                j+=1
+                j += 1
         elif i >= n1:
             newArr.append(arr2[j])
-            j+=1
+            j += 1
         else:
-            newArr.append(arr1[i])            
-            i+=1
-    
+            newArr.append(arr1[i])
+            i += 1
+
     return newArr
 # print(merge2sorted([1,4,5, 7], [2,3,4,6]))
 
-# Sliding window 
+# Sliding window
+
+
 def max_sum_subarray(arr, k):
     max_sum = -1
     start = 0
@@ -203,7 +219,10 @@ def max_sum_subarray(arr, k):
             start += 1
 
     return max_sum
-print(max_sum_subarray([2,3,4,1,5], 3))
+
+
+print(max_sum_subarray([2, 3, 4, 1, 5], 3))
+
 
 def smallest_size_subarray(arr, k):
     min_size = -1
@@ -212,12 +231,16 @@ def smallest_size_subarray(arr, k):
     for end, val in enumerate(arr):
         cur_sum += val
         while cur_sum >= k:
-            min_size = end - start + 1 if min_size == -1 else min(min_size, end - start + 1)
+            min_size = end - start + 1 if min_size == - \
+                1 else min(min_size, end - start + 1)
             cur_sum -= arr[start]
             start += 1
 
     return min_size
-print(smallest_size_subarray([2,4,2,5,1], 7))
+
+
+print(smallest_size_subarray([2, 4, 2, 5, 1], 7))
+
 
 def find_max_price(price_arr, weight_arr, max_weight):
     max_price = float('-inf')
@@ -232,22 +255,76 @@ def find_max_price(price_arr, weight_arr, max_weight):
             cur_weight -= weight_arr[start]
             cur_price -= price_arr[start]
             start += 1
-        
+
         max_price = max(cur_price, max_price)
-        
+
     return max_price
-print(find_max_price([3000, 3000,2000, 1500], [36, 30, 20, 15], 35))
 
-# collections
-q = collections.deque()
-s = set()
-m = dict()
-s.add(1)
-m['1'] = 1
-if 1 in s:
-    print('abc')
 
-if '1' in m:
-    print('haha')
+print(find_max_price([3000, 3000, 2000, 1500], [36, 30, 20, 15], 35))
 
-#[[0] * m for i in range(n)] matrix
+
+def longestPalindromeDp(s):
+    rev = s[::-1]
+    n = len(s)
+    dp = [[0] * n for _ in range(n)]
+    max_len = 0
+    max_end = 0
+
+    for i in range(n):
+        for j in range(n):
+            if s[i] == rev[j]:
+                dp[i][j] = 1 + (dp[i-1][j-1] if i > 0 and j > 0 else 0)
+                cur += s[i]
+            else:
+                dp[i][j] = 0
+            if dp[i][j] > max_len and i - dp[i][j] + 1 == len(s) - 1 - j:
+                # start index of duplicated substring [x] should be same as index  on reversed string in revered order
+                # index x = 2 -> y = len(s) - 1 - x
+                max_len = dp[i][j]
+                max_end = i
+
+    return s[max_end - max_len + 1: max_end+1]
+# print(longestPalindrome("babad"))
+
+
+def removeinvalidparentheses(s):
+    invalid_indices = set()
+    parentheses = []
+
+    for i, c in enumerate(s):
+        if c == '(':
+            parentheses.append(i)
+        elif c == ')':
+            if len(parentheses) == 0:
+                invalid_indices.add(i)
+            else:
+                parentheses.pop()
+
+    res = ''
+    for i in range(len(s)):
+        if i not in parentheses and i not in invalid_indices:
+            res += s[i]
+
+    return res
+
+
+print(removeinvalidparentheses("lee(t(c)o)de)"))
+
+
+def isValidBTS(a):
+    min_val = -1
+    stack = []
+
+    for i in range(len(a)):
+        if a[i] < min_val:
+            return 'NO'
+        else:
+            while len(stack) > 0 and stack[-1] < a[i]:
+                min_val = stack.pop()
+            stack.append(a[i])
+
+    return 'YES'
+
+
+print(isValidBTS([1, 3, 4, 2]))
