307801b031f458c705c94bf2201592b68a2bb3b6
Brian Le <brian.le@grabtaxi.com>
2021-05-02 16:28:22 +0800

    cm

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# algo
+# Coding Interview Prep
+
+- [MIT 6.006 Introduction to Algorithms][MIT 6.006 Introduction to Algorithms]
+
+## [MIT 6.006 Introduction to Algorithms](https://youtube.com/playlist?list=PLUl4u3cNGP61Oq3tWYp6V_F-5jb5L2iHb)
+
+#### 1. Algorithmic Thinking, Peak finding
--- test.py
+++ test.py
@@ -331,3 +331,50 @@ def isValidBTS(a):
     return 'YES'
 
 # print(isValidBTS([1, 3, 4, 2]))
+# Definition for singly-linked list.
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if l1 != None and l2 is None:
+            return l1
+        elif l2 != None and l1 is None:
+            return l2
+        elif l1 is None and l2 is None:
+            return None
+        
+        head = None
+        if l1.val < l2.val: 
+            head = l1
+            l1 = l1.next
+        else:
+            head = l2
+            l2 = l2.next
+        head.next = self.mergeTwoLists(l1, l2)
+
+        return head
+
+    def longestCommonPrefix(self, strs: [str]) -> str:
+        res = ''
+        
+        for i in range(1, len(strs)):
+            prefix = self.commonPrefix(strs[i], strs[i-1])
+            if res == '' or len(prefix) < len(res):
+                res = prefix
+        
+        return res
+    
+    def commonPrefix(self, str1, str2):
+        n = min(len(str1), len(str2))
+        prefix = ''
+        for i in range(n):
+            if str1[i] != str2[i]:
+                break;
+            prefix += str1[i]
+        return prefix
+
+print(Solution().longestCommonPrefix(['dog', 'racecard', 'card']))
