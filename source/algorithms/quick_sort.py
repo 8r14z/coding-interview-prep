72b0ef5f57f2498fc4244560b24ea9d0278957c4
Brian Le <brian.le@grabtaxi.com>
2021-05-04 23:58:31 +0800

    cm

---
+++ source/algorithms/quick_sort.py
@@ -0,0 +1,20 @@
+def partition(array, left, right):
+    pivot = array[right]
+    i = left  # i is the last position where arra[i] > pivot
+    for j in range(left, right):
+        if array[j] <= pivot:
+            array[i], array[j] = array[j], array[i]  # swap arr[i] arr[j]
+            i += 1
+
+    array[i], array[right] = array[right], array[i]
+    return i
+
+def quicksort_helper(array, left, right):
+    if left < right:
+        pivotIndex = partition(array, left, right)
+        quicksort_helper(array, left, pivotIndex - 1)
+        quicksort_helper(array, pivotIndex + 1, right)
+
+def quicksort(array):
+    quicksort_helper(array, 0, len(array) - 1)
+
--- test.py
+++ test.py
@@ -42,27 +42,6 @@ def dfs(graph):
     return reached
 
 
-def partition(array, left, right):
-    pivot = array[right]
-    i = left  # i is the last position where arra[i] > pivot
-    for j in range(left, right):
-        if array[j] <= pivot:
-            array[i], array[j] = array[j], array[i]  # swap arr[i] arr[j]
-            i += 1
-
-    array[i], array[right] = array[right], array[i]
-    return i
-
-
-def quicksort(array):
-    quicksort_helper(array, 0, len(array) - 1)
-
-
-def quicksort_helper(array, left, right):
-    if left < right:
-        pivotIndex = partition(array, left, right)
-        quicksort_helper(array, left, pivotIndex - 1)
-        quicksort_helper(array, pivotIndex + 1, right)
 
 
 def insertion_sort(array):
