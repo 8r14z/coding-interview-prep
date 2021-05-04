03d2a75ad6cfea9ccafa5e41af2a9997e33089a9
Brian Le <brian.le@grabtaxi.com>
2021-05-04 23:59:03 +0800

    cm

--- test.py
+++ test.py
@@ -44,14 +44,6 @@ def dfs(graph):
 
 
 
-def insertion_sort(array):
-    for i in range(len(array)):
-        j = i
-        while j > 0 and array[j] < array[j-1]:
-            array[j], array[j-1] = array[j-1], array[j]
-            j -= 1
-    return array
-
 
 def selection_sort(array):
     for i in range(len(array)):
