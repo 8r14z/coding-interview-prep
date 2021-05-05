ceea017dd5ca2c909997cadf52b9779f4a531435
Brian Le <brian.le@grabtaxi.com>
2021-05-05 15:18:26 +0800

    cm

--- test.py
+++ test.py
@@ -1,26 +1,6 @@
 import collections
 
-#   1 
-#     3
-#      4
-#     2
-# invalid BTS 2 < 3
-# input is an array that presents BTS in pre-order
-def isValidBTS(a):
-    min_val = -1
-    stack = []
 
-    for i in range(len(a)):
-        if a[i] < min_val:
-            return 'NO'
-        else:
-            while len(stack) > 0 and stack[-1] < a[i]:
-                min_val = stack.pop()
-            stack.append(a[i])
-
-    return 'YES'
-
-# print(isValidBTS([1, 3, 4, 2]))
 # Definition for singly-linked list.
 
 
