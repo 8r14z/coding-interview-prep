9ecd5bcef8fd542934a9e281c0cc07a931d7c98e
Brian Le <brian.le@grabtaxi.com>
2021-05-05 15:06:38 +0800

    cm

--- test.py
+++ test.py
@@ -3,19 +3,6 @@ from source.algorithms.quick_sort import partition
 
 
 
-def longest_common_subsequence_dp(str1, str2):
-    res = 0
-    dp = [[0] * (len(str2) + 1) for _ in range(len(str1) + 1)]
-
-    for i in range(1, len(str1)+1):
-        for j in range(1, len(str2)+1):
-            if str1[i-1] == str2[j-1]:
-                dp[i][j] = 1 + dp[i-1][j-1]
-            else:
-                dp[i][j] = max(dp[i][j-1], dp[i-1][j])
-            res = max(res, dp[i][j])
-
-    return res
 
 # print(longest_common_subsequence_dp('fort', 'fosh')) # a d s
 
