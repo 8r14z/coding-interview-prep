ad4b0570437b2d1995c3668a00359ff9bbdcd262
Brian Le <brian.le@grabtaxi.com>
2021-05-05 15:20:37 +0800

    cm

---
+++ source/problems/merge_two_linkedlist.py
@@ -0,0 +1,23 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if l1 != None and l2 is None:
+            return l1
+        elif l2 != None and l1 is None:
+            return l2
+        elif l1 is None and l2 is None:
+            return None
+        
+        head = None
+        if l1.val < l2.val: 
+            head = l1
+            l1 = l1.next
+        else:
+            head = l2
+            l2 = l2.next
+        head.next = self.mergeTwoLists(l1, l2)
+
+        return head
