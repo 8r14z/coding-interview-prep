35ec41e35d8111ab7bb7a728537ee9229ee71a5c
Brian Le <brian.le@grabtaxi.com>
2021-05-10 15:34:36 +0800

    cm

--- source/problems/circular_array_loop.py
+++ source/problems/circular_array_loop.py
@@ -2,30 +2,27 @@ class Solution:
     def nextIndex(self, nums: [int], index: int) -> int:
         n = len(nums)
         next = index + nums[index]
-        if next > n-1:
+        if next > n-1 or next < 0:
             return next % n
-        elif next < 0:
-            return n - (next * -1) % 6
         else:
             return next
 
     def circularArrayLoop(self, nums: [int]) -> bool:
         n = len(nums)
+    
         for i in range(n):
-            k = 0
-            steps = 0
             curIdx = i
-            while steps < n:
-                if steps != 0 and abs(steps) > abs(steps + nums[curIdx]): 
+            visited = set()
+            while curIdx not in visited:
+                if (nums[i] > 0 and nums[curIdx] < 0) or (nums[i] < 0 and nums[curIdx] > 0):
                     break
 
+                visited.add(curIdx)
+                curIdx = self.nextIndex(nums, curIdx)
 
-            if steps == n and k > 1:
+            if curIdx == i and len(visited) > 1:
                 return True
 
         return False
 
-
-# 0 1(-2) 2 3 4 5
-
-print(Solution().circularArrayLoop([2,-1,1,2,2]))
+print(Solution().circularArrayLoop([-2,-17,-1,-2,-2]))
