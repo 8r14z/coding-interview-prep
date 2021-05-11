ac8342cad9a1308f7caef1bed1ab69c981e16ff8
Brian Le <brian.le@grabtaxi.com>
2021-05-11 14:40:30 +0800

    cm

--- source/algorithms/radix_sort.py
+++ source/algorithms/radix_sort.py
@@ -1,19 +1,55 @@
 import math
 def countingSort(nums: [int], d: int, base: int):
-    count = [[]] * base
+    
+    # count = [[]] * base
 
+    # for num in nums:
+    #     countIndex = (num // (base ** (d-1))) % base
+    #     if len(count[countIndex]) == 0:
+    #         count[countIndex] = [num]
+    #     else:
+    #         count[countIndex].append(num)
+
+    # output = []
+    # for i in range(base):
+    #     if len(count[i]) > 0:
+    #         output += count[i]
+
+    # return output
+
+    # improvement - in-place position array
+    # [3,2,5,1,5,3,4] -> sorted [1,2,3,3,4,5,5]
+    # 1 - build pos array with num of occurrence of on each index
+    # [0,1,1,2,1,2]
+    # 2 - inplace transform to array of num of items that less than pos[i]
+    # [0,0,1,2,4,5] -> this is also the start index in the sorted array
+    # for example 5 in sorted array is started at index 5 
+    #  0 1 2 3 4 5 6 <- index
+    # [1,2,3,3,4,5,5]
+    # in the output array, get output[pos[num]] = num and increment post[num] by 1, 
+    # so the next time the same num is selected it will be slotted in the next index
+    # for example: output[pos[5]] = 5, pos[5] += 1
+    #                      ^            ^
+    #                      5            6
+    n = base
+    pos = [0] * n
+    
+    for num in nums: 
+        countIndex = (num // (base ** (d-1))) % base
+        pos[countIndex] += 1
+    
+    numOfNums = len(nums)
+    for i in range(n-1, -1, -1):
+        numAtPos = numOfNums - pos[i]
+        pos[i] = numAtPos
+        numOfNums = numAtPos
+
+    output = [0] * len(nums)
     for num in nums:
         countIndex = (num // (base ** (d-1))) % base
-        if len(count[countIndex]) == 0:
-            count[countIndex] = [num]
-        else:
-            count[countIndex].append(num)
-
-    output = []
-    for i in range(base):
-        if len(count[i]) > 0:
-            output += count[i]
-
+        output[pos[countIndex]] = num
+        pos[countIndex] += 1
+    
     return output
 
 
