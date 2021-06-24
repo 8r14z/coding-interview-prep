f172b22f2704c04d939f6428d3a2c66fc70065c6
Brian Le <brian.le@grabtaxi.com>
2021-06-25 00:07:55 +0800

    cm

--- source/problems/coin_change.py
+++ source/problems/coin_change.py
@@ -1,6 +1,8 @@
 # calculate number of ways
 # min/max numbe of coins will similar to unbouned knapsack [implement](./knapsack.py)
 
+# coins is a list of coins with different value 
+# n is the target amount we wanna get change
 def num_of_ways(coins, n):
     m = len(coins)
     dp = [[0]*(n+1) for _ in range(m)]
@@ -20,6 +22,21 @@ def num_of_ways(coins, n):
             dp[i][j] = num_including_coin_set + num_excluding_coin_set
     
     return dp[m-1][n]
+
+def num_of_ways_optimal_space(coins, n):
+    m = len(coins)
+    if m < 1: return 0
+    if n < 1: return 1
+    res = 0
+
+    dp = [0] * (n+1)
+    dp[0] = 1
+
+    for coin in coins:
+        for amount in range(1, n+1):
+            if coin <= amount:
+                dp[amount] += dp[amount - coin]
+    return dp[n]
  
 # this particular problem need a coin value == 1 to be solvable
 def minimum_num_of_coins(coins, n):
@@ -41,5 +58,10 @@ print(num_of_ways([1,2,3], 4)) # 4
 print(num_of_ways([1, 5, 10], 12)) # 4
 print(num_of_ways([2, 5, 3, 6], 10)) # 5
 print(num_of_ways([1, 5, 10], 10)) # 4
+
+assert(num_of_ways_optimal_space([1,2,3], 4) == 4) # 4
+assert(num_of_ways_optimal_space([1, 5, 10], 12) == 4) # 4
+assert(num_of_ways_optimal_space([2, 5, 3, 6], 10) == 5) # 5
+assert(num_of_ways_optimal_space([1, 5, 10], 10) == 4) # 4
 
 print(minimum_num_of_coins([1,2,5], 11))
