56e62b83636d2744c65b07d91911d06cca4ba21a
Brian Le <brian.le@grabtaxi.com>
2021-06-28 23:04:17 +0800

    cm

--- companies/amzn/amzn.py
+++ companies/amzn/amzn.py
@@ -1,39 +1,5 @@
-Phone interview: the same process is applied for all roles :) doesn't matter what kind of role u apply for...
-
-- leveling is done by Amazon :) 
-- no need to turn on camera 
-- collaborative: solve with the engineer
-- 35-40m coding 20m 5 Q&A
-- 1 or 2 coding problems - computer science fundamentals
-- any languages :) 
-- ask clarify questions (always) -> thinking through -> communicate through (why, edge cases) -> start implementing -> clean code -> testing with couple of samples -> interview gives hints (guide to the right answer) -> no need to compile -> work on medium categories :) 
-
-https://livecode.amazon.jobs/session/8a7c8403-31dc-4262-b39e-f93935927ef7
-
-https://www.educative.io/blog/crack-amazon-coding-interview-questions. 
-
-https://www.youtube.com/watch?v=aOu7BaKgnMc
-
-https://www.amazon.jobs/en/landing_pages/phone-interview
-
-Behaviral:
-- past experience align to princicples
-- working project deadline
-- dive deep to solve problem 
-- bad feedbacks
-- did learn from failures :) 
-- grew or changed from the failures
-- big impacts :) 
-- 10 stories cover all scenarios
-- 2-3 behaviral questions :) 
-- STAR format
-- tangible result :) metrics, numbers, backed by data
-- how did u do differently? 
-
-=> within 2 days - 6 months
-
-```python
 # https://www.educative.io/blog/crack-amazon-coding-interview-questions
+
 import math
 from collections import deque
 from functools import lru_cache
@@ -357,4 +323,3 @@ def binary_search_rotated(arr, key):
         else:
             return mid
     return -1
-```
