56770c29e94863de81b9bb84feea29e73a90b7f5
Brian Le <brian.le@grabtaxi.com>
2021-07-18 21:11:15 +0800

    cm

---
+++ source/problems/leetcode/146_lru_cache.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/lru-cache/
+from collections import OrderedDict
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self._cache = OrderedDict()
+
+    def get(self, key: int) -> int:
+        if key in self._cache: 
+            self._cache.move_to_end(key)
+            return self._cache[key]
+        else:
+            return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self._cache:
+            self._cache.move_to_end(key)
+            
+        self._cache[key] = value
+        if len(self._cache) > self.capacity:
+            self._cache.popitem(last = False)
