ac68bd3d03d01ceba27c7214d4774224bc01a82a
Brian Le <brian.le@grabtaxi.com>
2021-07-26 09:06:18 +0800

    cm

---
+++ source/data_structure/bloom_filter.py
@@ -0,0 +1,11 @@
+# https://www.geeksforgeeks.org/bloom-filters-introduction-and-python-implementation/
+
+import mmh3
+import math
+from bitarray import bitarray
+
+class BloomFilter:
+    def __init__(self, item_count, prob):
+        self.item_count = item_count
+        self.prob = prob
+        
