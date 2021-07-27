d08cc86fea4be5e211556e3b29c2e92a8c0308d2
Brian Le <brian.le@grabtaxi.com>
2021-07-27 10:49:59 +0800

    cm

---
+++ source/problems/leetcode/199_binary_tree_right_side_view.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/binary-tree-right-side-view/
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def rightSideView(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+    
+        queue = deque([root])
+        answer = []
+        while queue:
+            q_size = len(queue)
+            node = None
+            for _ in range(q_size):
+                node = queue.popleft()
+                
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+                
+            if node:1
+                answer.append(node.val)
+                
+        return answer
+                
