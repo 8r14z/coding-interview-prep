6b259bc4b0e648fc5b6d352d0f2f4463915d0626
Brian Le <brian.le@grabtaxi.com>
2021-08-01 17:08:38 +0800

    cm

---
+++ source/problems/leetcode/332_reconstruct_itinerary.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/reconstruct-itinerary/
+
+class Solution(object):
+    def findItinerary(self, tickets):
+    
+        def build_graph(tickets):
+            graph = defaultdict(list)
+            for ticket in tickets:
+                origin, dest = ticket
+                graph[origin].append(dest)
+
+            for origin in graph:
+                graph[origin].sort(reverse=True)
+            
+            return graph
+        
+        def dfs(graph, source, order):
+            while graph[source]:
+                dest = graph[source].pop()
+                dfs(graph, dest, order)
+            
+            order.append(source)
+        
+        graph = build_graph(tickets)
+        output = []
+        dfs(graph, 'JFK', output)
+        return output[::-1]
