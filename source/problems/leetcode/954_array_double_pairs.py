4b8290cc54e438b7579a0bec6995e0621cd04706
Brian Le <brian.le@grabtaxi.com>
2021-08-12 14:06:47 +0800

    cm

---
+++ source/problems/leetcode/954_array_double_pairs.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/array-of-doubled-pairs/
+
+class Solution:
+    def canReorderDoubled(self, arr: List[int]) -> bool:
+        arr.sort(key=abs)
+        counter = Counter(arr)
+        
+        for num in arr:
+            if counter[num] == 0:
+                continue
+            if counter[2*num] == 0:
+                return False
+            
+            counter[num] -= 1
+            counter[2*num] -= 1
+            
+        return True
+        
+        
+# sort increasing by abs
+# -2 2 -4 4
+# if the array is valid, there is always a pair of num and 2*num
+
