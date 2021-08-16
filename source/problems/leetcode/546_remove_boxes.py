4bf9b9aade4bd01eeccc4e3d3e320426569cc303
8R!4N <lvhan.clc@gmail.com>
2021-08-16 15:58:02 +0800

    cm

--- source/problems/leetcode/546_remove_boxes.py
+++ source/problems/leetcode/546_remove_boxes.py
@@ -11,6 +11,7 @@ class Solution:
             while m+1 <= j and A[m+1] == A[i]:
                 m += 1
             i, k = m, k + m - i
+            
             ans = dp(i+1, j, 0) + (k+1) ** 2
             for m in range(i+1, j+1):
                 if A[i] == A[m]:
