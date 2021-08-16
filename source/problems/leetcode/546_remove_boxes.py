f65bef8f429023c5c6326ac57d0b2a41e55841eb
8R!4N <lvhan.clc@gmail.com>
2021-08-16 17:08:34 +0800

    cm

--- source/problems/leetcode/546_remove_boxes.py
+++ source/problems/leetcode/546_remove_boxes.py
@@ -1,21 +1,24 @@
 # https://leetcode.com/problems/remove-boxes/
 
 class Solution:
-    def removeBoxes(self, A):
-        N = len(A)
+    def removeBoxes(self, boxes: List[int]) -> int:
+        N = len(boxes)
         
         @lru_cache(None)
-        def dp(i, j, k):
-            if i > j: return 0
-            m = i
-            while m+1 <= j and A[m+1] == A[i]:
-                m += 1
-            i, k = m, k + m - i
+        def dp(l,r,count):
+            if l > r: return 0
+            elif l == r: return (count+1)**2
             
-            ans = dp(i+1, j, 0) + (k+1) ** 2
-            for m in range(i+1, j+1):
-                if A[i] == A[m]:
-                    ans = max(ans, dp(i+1, m-1, 0) + dp(m, j, k+1))
+            m = l
+            while m <= r and boxes[m] == boxes[l]:
+                m += 1
+            
+            count = count + m - l
+            ans = dp(m, r, 0) + count**2
+            
+            for i in range(m, r+1):
+                if boxes[i] == boxes[l]:
+                    ans = max(ans, dp(m,i-1,0) + dp(i,r, count))
             return ans
-
+        
         return dp(0, N-1, 0)
