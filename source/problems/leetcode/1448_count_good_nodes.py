fb31f58fe264f8201bd020764c8bcc25c5d693d3
8R!4N <lvhan.clc@gmail.com>
2021-08-18 15:18:10 +0800

    cm

--- source/problems/leetcode/1448_count_good_nodes.py
+++ source/problems/leetcode/1448_count_good_nodes.py
@@ -8,22 +8,19 @@ class TreeNode:
         self.right = right
 
 class Solution:
-    def goodNodes(self, root: TreeNode) -> int:
-        def dfs(node, val):
-            if not node:
-                return 0
-            
-            ans = 0
-            if node.val >= val:
-                ans = 1
-            
-            if node.left:
-                ans += dfs(node.left, max(node.val, val))
-            
-            if node.right:
-                ans += dfs(node.right, max(node.val, val))
-            
-            return ans
+    def goodNodes(self, root: TreeNode, max_val = float('-inf')) -> int:
+        if not root:
+            return 0
         
-        return dfs(root, float('-inf'))
+        ans = 0
+        if root.val >= max_val:
+            ans += 1
+            max_val = root.val
+        
+        if root.left:
+            ans += self.goodNodes(root.left, max_val)
+        if root.right:
+            ans += self.goodNodes(root.right, max_val)
+            
+        return ans
         
