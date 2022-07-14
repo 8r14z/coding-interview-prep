bc28e42e5d2a05b17e9f12c775c0c01d7def8da4
ble <ble@snapchat.com>
2022-07-14 01:06:53 +0100

    Brian has pushed some changes

--- source/problems/leetcode/300_longest_increasing_subseq.py
+++ source/problems/leetcode/300_longest_increasing_subseq.py
@@ -1,5 +1,6 @@
 # https://leetcode.com/problems/longest-increasing-subsequence/
 
+# Memorization
 INF = float('inf')
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
@@ -16,3 +17,41 @@ class Solution:
             answer = max(answer, dp[i])
              
         return answer
+
+# Binary search 
+
+class Solution:
+    def search(subsequence, val) -> int:
+        low = 0
+        high = len(subsequence) - 1
+
+        index = high
+        while low <= high:
+            mid = (low + high) // 2
+            if (subsequence[mid] >= val):
+                index = min(index, mid)
+                high = mid - 1
+            else:
+                low = mid + 1
+
+        return index
+
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        n = len(nums)
+        if not n:
+            return 0
+        if n == 1:
+            return 1
+        
+        subsequence = []
+        subsequence.append(nums[0])
+        
+        for i in range(1, n):
+            index = self.search(subsequence, nums[i])
+            if (index < len(subsequence)):
+                subsequence[index] = nums[i]
+            else:
+                subsequence.append(nums[i])
+        
+        
+        return len(subsequence)
