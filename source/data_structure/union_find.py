ea2d152e26c6b46814f3a378b46d6fa5fd28c513
ble <ble@snapchat.com>
2022-07-23 18:08:51 +0100

    Brian has pushed some changes

--- source/data_structure/union_find.py
+++ source/data_structure/union_find.py
@@ -1,5 +1,3 @@
-# Union Find == Disjoint Sets
-
 class UnionFind:
     # initialize the data structure that maps the node to its set ID
     def __init__(self):
@@ -7,11 +5,10 @@ class UnionFind:
 
     # find the Set ID of Node x
     def find(self, x):
-        # get the value associated with key x, if it's not in the map return x
         y = self.id.get(x, x)
         # check if the current node is a Set ID node
         if y != x:
-            # set the value to Set ID node of node y
+            # set the value to Set ID node of node y, and change the hash value of node x to Set ID value of node y
             self.id[x] = y = self.find(y)
         return y
 
