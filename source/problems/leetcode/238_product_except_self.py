4c89f39092eae5f047026675b3fbf83b4cf3383d
ble <ble@snapchat.com>
2022-07-26 18:35:07 +0100

    Brian has pushed some changes

--- source/problems/leetcode/238_product_except_self.py
+++ source/problems/leetcode/238_product_except_self.py
@@ -1,31 +1,23 @@
 # https://leetcode.com/problems/product-of-array-except-self/
 
 class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:        
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        
         n = len(nums)
-
-        if n < 2:
-            return []
+        if (n <= 1):
+            return nums
         
-        left = [0] * n
-        left[0] = nums[0]
+        ans = [0] * n
         
-        right = [0] * n
-        right[-1] = nums[-1]
-        
+        prefix = nums[0]
         for i in range(1, n):
-            left[i] = nums[i] * left[i-1]
+            ans[i] = prefix
+            prefix *= nums[i]
         
+        postfix = nums[-1]
         for i in reversed(range(n-1)):
-            right[i] = nums[i] * right[i+1]
-            
-        res = []
-        for i in range(n):
-            if i == 0:
-                res.append(right[i+1])
-            elif i == n-1:
-                res.append(left[i-1])
-            else:
-                res.append(left[i-1] * right[i+1])
+            ans[i] = postfix if i == 0 else ans[i] * postfix
+            postfix *= nums[i]
         
-        return res
+        return ans
+        
--- source/problems/leetcode/3_longgest_substring_without_repeating.py
+++ source/problems/leetcode/3_longgest_substring_without_repeating.py
@@ -19,24 +19,3 @@ class Solution:
             res = max(res, end-start+1)
 
         return res
-
-    # O(n) - O(1)
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
-        
-        n = len(nums)
-        if (n <= 1):
-            return nums
-        
-        ans = [0] * n
-        
-        prefix = nums[0]
-        for i in range(1, n):
-            ans[i] = prefix
-            prefix *= nums[i]
-        
-        postfix = nums[-1]
-        for i in reversed(range(n-1)):
-            ans[i] = postfix if i == 0 else ans[i] * postfix
-            postfix *= nums[i]
-        
-        return ans
