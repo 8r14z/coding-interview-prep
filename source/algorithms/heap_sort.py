c26dec802924ec0112d43a99a228095922fc7c36
Brian <lvhan.clc@gmail.com>
2023-08-01 00:49:32 +0100

    add comment build heap complexity

--- source/algorithms/heap_sort.py
+++ source/algorithms/heap_sort.py
@@ -29,3 +29,14 @@ print(heapSort([10,6,7,5,15,17,12])) # [5, 6, 7, 10, 12, 15, 17]
 
 # Build heap is O(n)
 # https://www.geeksforgeeks.org/time-complexity-of-building-a-heap/
+
+# https://stackoverflow.com/questions/9755721/how-can-building-a-heap-be-on-time-complexity
+# Intuitively, we can see infer the buildHeap algorithm as nlog(n), it's a upper bound but not tight bound
+# At cost of heapify at each step is different, depending on the high of subtree which is different at different levels
+# At leaf-node level, there are n/2 nodes and the high = 0 => cost = n/2 * 0
+# At upper level, there are n/2/2 = n/4 nodes and high = 1 => cost = n/4 * 1
+# ...
+# At the parent node, there is 1 node and high = log(n) = heigh of tree (h) => cost = 1 * h
+# If we keep moving upward the tree and do the heapify, we can build the series as
+# n/2 * 0 + n/4 * 1 + n/8 * 2 + ... + 1 * h
+# = h * n / 2^(h+1) with h range from (0, logn)
