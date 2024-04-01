89417aa6da5f8604abea14838a747a06e50842aa
Brian <lvhan.clc@gmail.com>
2024-04-01 13:42:55 +0100

    add meta 162

--- source/problems/meta/meta_lc_tag.py
+++ source/problems/meta/meta_lc_tag.py
@@ -472,4 +472,28 @@ class Solution:
                     if frequency[oc] == 0:
                         i += 1
         return res
-        
+
+# https://leetcode.com/problems/find-peak-element/        
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 1:
+            return 0
+        if nums[0] > nums[1]:
+            return 0
+        if nums[n-1] > nums[n-2]:
+            return n-1
+
+        low = 1
+        high = n-2
+        while low < high:
+            mid = (low + high) // 2
+            if nums[mid] < nums[mid+1]:
+                low = mid + 1
+            elif nums[mid] < nums[mid-1]:
+                high = mid - 1
+            else:
+                return mid
+
+        return low
+            
