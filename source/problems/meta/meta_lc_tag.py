1d3ad06b07c571572a4e3cb0ec900eea8b88a3d4
Brian <lvhan.clc@gmail.com>
2024-04-03 17:15:12 +0100

    add meta 986

--- source/problems/meta/meta_lc_tag.py
+++ source/problems/meta/meta_lc_tag.py
@@ -520,4 +520,21 @@ class Solution:
 
         return res
 
+# https://leetcode.com/problems/interval-list-intersections/
+class Solution:
+    def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
+        res = []
+        i = j = 0
+        while i < len(firstList) and j < len(secondList):
+            interval1 = firstList[i]
+            interval2 = secondList[j]
+            start = max(interval1[0], interval2[0])
+            end = min(interval1[1], interval2[1])
+            if start <= end:
+                res.append([start,end])
+            if interval1[1] < interval2[1]:
+                i += 1
+            else:
+                j += 1
 
+        return res
