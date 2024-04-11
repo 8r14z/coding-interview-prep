042b058e626a5a182735b2093036e31ab2147b8e
ble <ble@snapchat.com>
2024-04-11 13:06:04 +0100

    add meta 987

--- source/problems/meta/meta_lc_tag.py
+++ source/problems/meta/meta_lc_tag.py
@@ -968,3 +968,44 @@ class TicTacToe:
 #         return ans == -1 ? n*m : ans
 #     }
 # }
+
+# https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/
+# the only difference with the prev problem is to sort the col by row and col
+from collections import deque
+from collections import defaultdict 
+
+class Solution:
+    def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+
+        colMap = defaultdict(list)
+        
+        queue = deque()
+        queue.append((root,0,0))
+
+        minCol = float('inf')
+        maxCol = float('-inf')
+
+        while queue:
+            node, row, col = queue.popleft()
+            minCol = min(minCol, col)
+            maxCol = max(maxCol, col)
+
+            colMap[col].append((node.val, row))
+            
+            if node.left:
+                queue.append((node.left, row+1, col-1))
+            if node.right:
+                queue.append((node.right, row+1, col+1))
+        
+        res = []
+
+        for col in range(minCol, maxCol + 1):
+            if col not in colMap:
+                continue
+            items = sorted(colMap[col], key=lambda x: (x[1], x[0]))
+            vals = [val for val, _ in items]
+            res.append(vals)
+            
+        return res
