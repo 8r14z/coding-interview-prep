7b8b2c4a29c885c1841e99c56a1f8834486b74b3
Brian <lvhan.clc@gmail.com>
2024-05-02 15:41:44 +0100

    add meta 88

--- source/problems/meta/meta_lc_tag.py
+++ source/problems/meta/meta_lc_tag.py
@@ -873,6 +873,25 @@ class Solution:
 
         return ans
 
+# https://leetcode.com/problems/merge-sorted-array/
+# class Solution {
+#     func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+#         var i = m - 1
+#         var j = n - 1
+#         var z = nums1.count - 1
+#         while j >= 0 {
+#             if i >= 0 && nums1[i] > nums2[j] {
+#                 nums1[z] = nums1[i]
+#                 i -= 1
+#             } else {
+#                 nums1[z] = nums2[j]
+#                 j -= 1
+#             }
+#             z -= 1
+#         }
+#     }
+# }
+
 # https://leetcode.com/problems/diagonal-traverse/
 # BFS level by level
 class Solution:
