e70ddf46abba1fa78a154e05e5adc131197cf5ca
Brian <lvhan.clc@gmail.com>
2024-05-02 23:52:52 +0100

    add meta 766

--- source/problems/meta/meta_lc_tag.py
+++ source/problems/meta/meta_lc_tag.py
@@ -919,6 +919,23 @@ class Solution:
             leftToRight = not leftToRight
 
         return ans
+
+# https://leetcode.com/problems/toeplitz-matrix/
+class Solution {
+    func isToeplitzMatrix(_ matrix: [[Int]]) -> Bool {
+        let m = matrix.count
+        let n = matrix[0].count
+        
+        for i in 1..<m {
+            for j in 1..<n {
+                if matrix[i][j] != matrix[i-1][j-1] {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+}
         
 # https://leetcode.com/problems/closest-binary-search-tree-value/
 class Solution:
