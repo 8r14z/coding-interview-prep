033e5e91dc882fc485eb7f1e5290a3eaa1ad7d85
brian <lvhan.clc@gmail.com>
2025-07-07 13:28:34 +0100

    Update binary_tree.py

--- source/data_structure/binary_tree.py
+++ source/data_structure/binary_tree.py
@@ -1,7 +1,9 @@
+from typing import Optional
+
 class Node:
     def __init__(self, data):
-        self.left = None
-        self.right = None
+        self.left: Optional[Node] = None
+        self.right: Optional[Node] = None
         self.val = data
 
 node1 = Node(1)
@@ -15,7 +17,7 @@ node2.right = node4
 node4.left = node3
 node4.right = node5
 
-def interative_inorder(node) -> [int]:
+def interative_inorder(node) -> list[int]:
     res = []
     stack = []
     cur = node
@@ -31,7 +33,7 @@ def interative_inorder(node) -> [int]:
             
     return res 
 
-def recursive_inorder(node) -> [int]:
+def recursive_inorder(node) -> list[int]:
     if node is None: return []
     return recursive_inorder(node.left) + [node.val] + recursive_inorder(node.right) 
 
